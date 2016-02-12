c4c4cc7064d926d5f025370943ec1e945bf050b9
Ale Rimoldi <ale@graphicslab.org>
2016-02-12 11:03:28 +0100

    add a first try at tests

---
+++ app/src/androidTest/java/ch/ideale/sunneschii/AdressBookReaderTest.java
@@ -0,0 +1,13 @@
+package ch.ideale.sunneschii;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by ale on 2/6/16.
+ */
+public class AdressBookReaderTest extends TestCase {
+
+    public void testGetAddresses() throws Exception {
+
+    }
+}
---
+++ app/src/main/java/ch/ideale/sunneschii/AdressBookReader.java
@@ -0,0 +1,12 @@
+package ch.ideale.sunneschii;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ale on 2/6/16.
+ */
+public class AdressBookReader {
+    public String[] getAddresses() {
+
+    }
+}
